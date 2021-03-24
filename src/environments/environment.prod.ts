424281f3bd683c239ab10839fb690a640635cc01
angeldef <angeldef.cc@gmail.com>
2021-03-24 14:06:18 -0500

    added prod env for build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
+
 export const environment = {
-  production: true
+   production: true,
+   baseURL: "https://6mx1tdn5jb.execute-api.us-east-2.amazonaws.com/dev",
+   loginURL: "https://d3eyeduwkwyhna.cloudfront.net/login"
 };
