6622df0b2a7518b961d2525bf5f8e6f353e71ed4
Jose Ochoa <joseochoa227@gmail.com>
2021-04-14 20:20:07 -0500

    Validate no blocks

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -11,7 +11,7 @@ import { ErrorService } from '../shared/error/error.service';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.scss']
+  styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
   backgroundImage: string;
@@ -41,12 +41,12 @@ export class EventosComponent implements OnInit {
       answer: null,
       question: null,
       question_pool: null,
-      survey: null
+      survey: null,
     };
   }
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       if (
         params['token'] != undefined &&
         params['id'] != undefined &&
@@ -86,25 +86,25 @@ export class EventosComponent implements OnInit {
       method: 'POST',
       headers: {
         Authorization: 'Bearer ' + token,
-        Accept: '*/*'
+        Accept: '*/*',
       },
-      body: cookieID
+      body: cookieID,
     })
-      .then(response => response.json())
+      .then((response) => response.json())
       .then(
-        data => {
+        (data) => {
           if (data.isAuth) {
             localStorage.setItem(
               'objTVA',
               JSON.stringify({
                 token: token,
                 cookieID: cookieID,
-                eventID: id
+                eventID: id,
               })
             );
 
             this._apiService.getEventById(id, token).subscribe(
-              response => {
+              (response) => {
                 console.log(response);
 
                 let w = window.innerWidth;
@@ -150,10 +150,14 @@ export class EventosComponent implements OnInit {
                   }
                 }
                 console.log(baux);
-
-                this.firtsTime(baux);
+                if (baux.length > 0) {
+                  this.firtsTime(baux);
+                } else {
+                  this.errorService.errorText = 'Página no encontrada';
+                  this.router.navigate(['error']);
+                }
               },
-              err => {
+              (err) => {
                 if (
                   err.error.error ==
                   'The current date is not in the date range of the event.'
@@ -178,7 +182,7 @@ export class EventosComponent implements OnInit {
             window.location.href = environment.loginURL;
           }
         },
-        error => {
+        (error) => {
           window.location.href = environment.loginURL;
         }
       );
@@ -216,10 +220,10 @@ export class EventosComponent implements OnInit {
           this._apiService
             .UrlEndByEventUser(this.myEvent, this.myToken)
             .subscribe(
-              response => {
+              (response) => {
                 goToUrl = response.redirect;
               },
-              error => {
+              (error) => {
                 console.log(error);
               },
               () => {
@@ -254,7 +258,7 @@ export class EventosComponent implements OnInit {
           description_qp: this.blocks[this.actualStep].config.description_qp,
           name_qp: this.blocks[this.actualStep].config.name_qp,
           question_pools_id: this.blocks[this.actualStep].config
-            .question_pools_id
+            .question_pools_id,
         };
 
         questionJson['question_id'] = this.blocks[this.actualStep].questions[
@@ -269,17 +273,17 @@ export class EventosComponent implements OnInit {
           event_id: eventIdJson,
           answer: answerJson,
           question: questionJson,
-          question_pool: question_poolJson
+          question_pool: question_poolJson,
         };
         console.log(jsonFinal);
 
         this.waitingForApi = true;
 
         this._apiService.saveSurvey(jsonFinal, this.myToken).subscribe(
-          response => {
+          (response) => {
             console.log(response);
           },
-          error => {
+          (error) => {
             console.log(error);
           },
           () => {
@@ -291,7 +295,7 @@ export class EventosComponent implements OnInit {
               let actions = {
                 name: 'SESSION_0',
                 type: this.blocks[this.actualStep].type,
-                step: 0
+                step: 0,
               };
               this.actions(actions);
               actions.name = 'SESSION_1';
@@ -307,11 +311,11 @@ export class EventosComponent implements OnInit {
       case 'SAVE_ELECTION':
         let json = {
           event_id: this.myEvent,
-          candidates: action.data
+          candidates: action.data,
         };
 
         this._apiService.saveElection(json, this.myToken).subscribe(
-          response => {
+          (response) => {
             console.log(response);
 
             swal
@@ -323,13 +327,13 @@ export class EventosComponent implements OnInit {
                 confirmButtonColor: '#332255',
                 cancelButtonColor: '#d33',
                 confirmButtonText: 'Ok',
-                cancelButtonText: 'Cancelar'
+                cancelButtonText: 'Cancelar',
               })
-              .then(result => {
+              .then((result) => {
                 this.actions({ name: 'NEXT' });
               });
           },
-          error => {
+          (error) => {
             console.log(error);
           }
         );
@@ -342,7 +346,7 @@ export class EventosComponent implements OnInit {
             session_id: this.session_id,
             step: `event/${this.myEvent}/${this.actualStep}/${action.type}/${action.step}`,
             event_id: this.myEvent,
-            status: 0
+            status: 0,
           };
           this.saveSession(json, this.myToken);
         } else {
@@ -350,7 +354,7 @@ export class EventosComponent implements OnInit {
             session_id: this.session_id,
             step: `event/${this.myEvent}/${this.actualStep}/${action.type}`,
             event_id: this.myEvent,
-            status: 0
+            status: 0,
           };
           this.saveSession(json, this.myToken);
         }
@@ -362,7 +366,7 @@ export class EventosComponent implements OnInit {
             session_id: this.session_id,
             step: `event/${this.myEvent}/${this.actualStep}/${action.type}/${action.step}`,
             event_id: this.myEvent,
-            status: 1
+            status: 1,
           };
           this.saveSession(json, this.myToken);
         } else {
@@ -370,7 +374,7 @@ export class EventosComponent implements OnInit {
             session_id: this.session_id,
             step: `event/${this.myEvent}/${this.actualStep}/${action.type}`,
             event_id: this.myEvent,
-            status: 1
+            status: 1,
           };
           this.saveSession(json, this.myToken);
         }
@@ -384,7 +388,7 @@ export class EventosComponent implements OnInit {
       (response: any) => {
         this.session_id = response.session_id;
       },
-      error => {
+      (error) => {
         console.log(error);
       }
     );
@@ -400,7 +404,7 @@ export class EventosComponent implements OnInit {
         session_id: this.session_id,
         step: `event/${this.myEvent}/${this.actualStep}/surveys/0`,
         event_id: this.myEvent,
-        status: 0
+        status: 0,
       };
       this._apiService.saveSession(json, this.myToken).subscribe(
         (response: any) => {
@@ -414,7 +418,7 @@ export class EventosComponent implements OnInit {
 
           this.totalSteps = this.blocks.length;
         },
-        error => {
+        (error) => {
           console.log(error);
         }
       );
@@ -424,10 +428,10 @@ export class EventosComponent implements OnInit {
         this._apiService
           .UrlEndByEventUser(this.myEvent, this.myToken)
           .subscribe(
-            response => {
+            (response) => {
               goToUrl = response.redirect;
             },
-            error => {
+            (error) => {
               console.log(error);
             },
             () => {
@@ -461,13 +465,13 @@ export class EventosComponent implements OnInit {
         b[this.actualStep].type
       }`,
       event_id: this.myEvent,
-      status: 0
+      status: 0,
     };
     this._apiService.saveSession(json, this.myToken).subscribe(
       (response: any) => {
         this.session_id = response.session_id;
       },
-      error => {
+      (error) => {
         console.log(error);
       },
       () => {
@@ -478,13 +482,13 @@ export class EventosComponent implements OnInit {
               b[this.actualStep].type
             }`,
             event_id: this.myEvent,
-            status: 1
+            status: 1,
           };
           this._apiService.saveSession(json, this.myToken).subscribe(
             (response: any) => {
               this.session_id = response.session_id;
             },
-            error => {
+            (error) => {
               console.log(error);
             },
             () => {
